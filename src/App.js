aba93469401d72bb4f005f7ce543ce7d326faf08
nienkebos <nienkebos@live.com>
2017-01-19 16:30:26 +0100

    Basic setup Header and Score

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Score from './containers/Score'
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <Score />
+      </div>
     )
   }
 }
 
-export default App
+export default App;
