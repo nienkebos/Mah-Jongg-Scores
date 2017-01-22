a3f664cc9da2e939dda502ff2a193c9f57f496e8
Koen Zwijnenburg <koen@3seconds.nl>
2017-01-22 17:43:25 +0100

    webpack configuration for sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./style/score.scss";
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <h1>Nienke is de slimste mens!</h1>
     )
   }
 }
 
-export default App
+export default App;
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: 'build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel-loader'] 
+      },
+      {
+        loaders:["style-loader", "css-loader", "sass-loader"],
+        test:/\.scss$/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
