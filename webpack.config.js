c4bdc00f20e630129e83e8b69715f14ba33808ae
Koen Zwijnenburg <koen@3seconds.nl>
2017-02-01 20:09:03 +0100

    fix redux connection

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,9 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    })
-  ]
+  //plugins: [
+  //  new HtmlWebpackPlugin({
+  //    template: './index.html'
+  //  })
+  //]
 };
